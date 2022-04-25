5546f6670903d6a3d1eed455290cc9a7909c660d
Mohd Irfan <moirfannizam@gmail.com>
2022-04-25 18:33:18 +0800

    fix homepage

--- module1/src/modules/Homepage.js
+++ module1/src/modules/Homepage.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { LoginContext } from '../contexts/LoginContext';
 
 function Homepage() {
@@ -17,30 +18,52 @@ function Homepage() {
     return (
         <div className="homepage container">
             <h2>Welcome to SmartBank credit card</h2>
-           
+
             <div className="user-detail">
-                <div>
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Credit Card Number</th>
-                                <th>Card Name</th>
-                                <th>User Name</th>
-                                <th>Available Points</th>
-                                <th>Total Rewards Gained (<i className="fas fa-rupee-sign"></i>)</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{loginContext.loggedInUser.ccNumber}</td>
-                                <td>{loginContext.loggedInUser.ccName}</td>
-                                <td>{loginContext.loggedInUser.userName}</td>
-                                <td>{loginContext.loggedInUser.availableRedeemPoints}</td>
-                                <td>{loginContext.loggedInUser.totalRewardsGained}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+
+                {
+                    loginContext.isLoggedIn ? 
+                        (
+                            <div>
+
+                                <table className="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Credit Card Number</th>
+                                            <th>Card Name</th>
+                                            <th>User Name</th>
+                                            <th>Available Points</th>
+                                            <th>Total Rewards Gained (<i className="fas fa-rupee-sign"></i>)</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>{loginContext.loggedInUser.ccNumber}</td>
+                                            <td>{loginContext.loggedInUser.ccName}</td>
+                                            <td>{loginContext.loggedInUser.userName}</td>
+                                            <td>{loginContext.loggedInUser.availableRedeemPoints}</td>
+                                            <td>{loginContext.loggedInUser.totalRewardsGained}</td>
+                                        </tr>
+                                    </tbody>
+
+
+
+                                </table>
+
+                            </div>
+                        ) :
+                        (
+                            <div className="mt-5">
+                                Please Sign in to redeem your points. &nbsp; &nbsp;
+                                <Link className="btn btn-primary" to="/login">
+                                    <i className="fas fa-user" />&nbsp; Sign in
+
+                                </Link>
+                            </div>
+                        )
+                }
+
+
             </div>
         </div>
     );
